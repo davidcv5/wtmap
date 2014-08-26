76b72ad7db90ec28cd653ab7a3bae9b799dc3ffb
David Cruz <davidcv5@gmail.com>
2014-08-26 01:59:44 -0400

    Adding OkHttpStack to use OkHttp with Volley. Quick sample on MainActivity.

--- app/src/main/java/com/challdoit/waitingmap/toolbox/OkHttpStack.java
+++ app/src/main/java/com/challdoit/waitingmap/toolbox/OkHttpStack.java
@@ -3,5 +3,33 @@ package com.challdoit.waitingmap.toolbox;
 /**
  * Created by David on 8/26/14.
  */
-public class OkHttpStack {
+import com.android.volley.toolbox.HurlStack;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * An {@link com.android.volley.toolbox.HttpStack HttpStack} implementation which
+ * uses OkHttp as its transport.
+ */
+public class OkHttpStack extends HurlStack {
+    private final OkUrlFactory mFactory;
+
+    public OkHttpStack() {
+        this(new OkHttpClient());
+    }
+
+    public OkHttpStack(OkHttpClient client) {
+        if (client == null) {
+            throw new NullPointerException("Client must not be null.");
+        }
+        mFactory = new OkUrlFactory(client);
+    }
+
+    @Override protected HttpURLConnection createConnection(URL url) throws IOException {
+        return mFactory.open(url);
+    }
 }
--- app/src/main/java/com/challdoit/waitingmap/ui/MainActivity.java
+++ app/src/main/java/com/challdoit/waitingmap/ui/MainActivity.java
@@ -4,16 +4,48 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.challdoit.waitingmap.R;
+import com.challdoit.waitingmap.toolbox.OkHttpStack;
 
 
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+        RequestQueue queue = Volley.newRequestQueue(this, new OkHttpStack());
+        String url = "http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 
 
